7bf3852a13636bce169b209c866b54ca32612bba
Astonark <sorel.kyrian@gmail.com>
2022-04-25 17:52:12 +0200

    Test unitairs pour la création, modification et suppréssion d'une category

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Feature;
 
-use App\Models\Film;
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -24,5 +25,28 @@ class ExampleTest extends TestCase
         $response = $this->get('films');
 
         $response->assertStatus(200);
+    }
+
+    public function test_create_category() {
+        $totalCategory = DB::table('categories')->count();
+        $expectedTotal = $totalCategory + 1;
+        $category = Category::create(['name' => 'catégorie test uniataire']);
+        $this->assertEquals($expectedTotal, Category::count());
+        return $category->id;
+    }
+
+    public function test_edit_category() {
+        $category = Category::find(22);
+        $category->update(['name' => 'modification du nom de la catégorie']);
+        $categoryName = $category->name;
+        $this->assertEquals('modification du nom de la catégorie', $categoryName);
+    }
+
+    public function test_delete_category() {
+        $category = Category::find(22);
+        $category->delete();
+        $this->assertDatabaseMissing('categories', [
+            'id' => '22'
+        ]);
     }
 }
